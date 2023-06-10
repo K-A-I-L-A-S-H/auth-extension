2b9fc25811997df8eb26fd25e9a04843937adbc3
Kailash Bisht <krukailash8501@gmail.com>
2023-06-10 20:21:51 +0530

    (chore): setup the resources

---
+++ src/api/coffees/index.ts
@@ -0,0 +1,3 @@
+export * from './coffees.controller';
+export * from './coffees.module';
+export * from './coffees.service';
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app';
+import { ConfigService } from '@nestjs/config';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
+import { json, urlencoded } from 'express';
+import { ApiVersions } from './constants';
+import { PrismaService } from 'lib/prisma';
 
 async function bootstrap() {
+  const logger = new Logger('NestApplicationLogger');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));
+
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ApiVersions.V1,
+  });
+  app.enableCors();
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHook(app);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  await app.listen(port);
+  logger.log(`Listening on port ${port}...`);
 }
+
 bootstrap();
