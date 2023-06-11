a068e2c1c2d8f3f2796a945ede72478b66d22dec
Kailash Bisht <krukailash8501@gmail.com>
2023-06-11 16:59:53 +0530

    (chore): add redis module and services

---
+++ lib/redis/redis.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Redis } from 'ioredis';
+
+@Injectable()
+export class RedisService {
+  private redisClient: Redis;
+  constructor(private readonly configService: ConfigService) {
+    this.redisClient = new Redis({
+      host: this.configService.get('REDIS_HOST'),
+      port: this.configService.get('REDIS_PORT'),
+    });
+  }
+
+  shutDown() {
+    return this.redisClient.quit();
+  }
+
+  async set(key: string, value: string) {
+    await this.redisClient.set(key, value);
+  }
+
+  async get(key: string) {
+    return this.redisClient.get(key);
+  }
+
+  async remove(key: string) {
+    await this.redisClient.del(key);
+  }
+}
